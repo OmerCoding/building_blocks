4af6b0f60d50d25ac93d95e9d71d1c2efb880f68
Omer Hefets <omerh127@gmail.com>
2017-09-01 18:02:34 +0300

    more readable code, added UI

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,29 +1,34 @@
-def caesar_cipher (string, shift_num)
-  @c_string = string
-  @final = ""
-  for pos in 0..string.length - 1
-    @char = @c_string[pos].ord
+LET_NUM = 26
 
-    if @char >= 65 && @char <= 90
-      @char += shift_num
-      if @char > 90
-        while @char > 90 do
-          @char -= 26
-        end
-      end
-    end
-    if @char >= 97 && @char <= 122
-      @char += shift_num
-      if @char > 122
-        while @char > 122 do
-          @char -= 26
-        end
-      end
-    end
-    @char = @char.chr
-    @final.concat(@char)
+def chip (start, stop, c, shift)
+  c += shift
+  while c > stop
+    c -= LET_NUM
   end
-  return @final
+  return c
 end
 
-puts caesar_cipher("Hello!5", 200)
+def caesar_cipher (string, shift_num)
+  c_string = string
+  final = ""
+  for pos in 0..string.length - 1
+    char = c_string[pos].ord
+    if char >= 60 && char <= 90
+      char = chip(65,90,char,shift_num)
+    end
+    if char >= 97 && char <= 122
+      char = chip(97,122,char,shift_num)
+    end
+    char = char.chr
+    final.concat(char)
+  end
+  return final
+end
+
+puts "Enter your string: "
+s = gets.chomp
+
+puts "Enter the requested shift: "
+shift = gets.to_i
+
+puts "Your encrypted string is: " + caesar_cipher(s, shift)
